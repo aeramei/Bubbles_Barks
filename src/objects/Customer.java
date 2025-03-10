894e64985a820b96b4ad7431e24ab52fd16aa71a
Ingyin Khine <ingyin.khine.fb.2020.2021@gmail.com>
2025-03-10 12:44:57 +0630

    feat: customer data type

--- src/objects/Customer.java
+++ src/objects/Customer.java
@@ -4,16 +4,38 @@ public class Customer {
     public String name;
     public String contactNumber;
     public PetType petType;
+    public ServiceType serviceType;
 
-
-    public Customer(String name, String contactNumber, PetType petType) {
+    public Customer(String name, String contactNumber, PetType petType, ServiceType serviceType) {
         this.name = name;
         this.contactNumber = contactNumber;
         this.petType = petType;
+        this.serviceType = serviceType;
     }
 
+    // Method to display customer information
     public void displayInfo() {
-        System.out.println("objects.Customer Name: " + name);
-        System.out.println("Contact Number: " + contactNumber);
+        System.out.println("===================================");
+        System.out.println("        CUSTOMER INFORMATION       ");
+        System.out.println("===================================");
+        System.out.println("Name:            " + name);
+        System.out.println("Contact Number:  " + contactNumber);
+        System.out.println("Pet Type:        " + petType);
+        System.out.println("Service Type:    " + serviceType);
+        System.out.println("===================================");
+    }
+
+    // Method to display a voucher for the service
+    public void displayVoucher(ServiceAndPrice serviceAndPrice) {
+        System.out.println("\n===================================");
+        System.out.println("           SERVICE VOUCHER         ");
+        System.out.println("===================================");
+        System.out.println("Customer Name:   " + name);
+        System.out.println("Pet Type:        " + petType);
+        System.out.println("Service:         " + serviceType);
+        System.out.println("Price:          $" + serviceAndPrice.getPrice(serviceType));
+        System.out.println("-----------------------------------");
+        System.out.println("Thank you for choosing our service!");
+        System.out.println("===================================");
     }
 }
